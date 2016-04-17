6812177a40335a3d891550d8afd084d5ff1b7fc2
root <root@localhost.localdomain>
2016-04-17 09:12:59 -0400

    add example

---
+++ example/src/main/java/HelloSpark.java
@@ -0,0 +1,9 @@
+import static spark.Spark.*;
+
+public class HelloSpark {
+    public static void main(String[] args) {
+        // JavaSparkContext spark = new JavaSparkContext("local", "MoneyCount");
+        System.out.println("hello");
+    }
+}
+
